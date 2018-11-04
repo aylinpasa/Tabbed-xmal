8f58fdfcabcf5480e53a411127f50d01915c1832
aylinpasa <32267023+aylinpasa@users.noreply.github.com>
2018-11-04 23:06:13 +0300

    Add files via upload

---
+++ Tabbed/TabbedPage1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace Homework1.Tabbed
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : TabbedPage
+    {
+        private object MainListView;
+
+        public TabbedPage1 ()
+        {
+            InitializeComponent();
+     
+           
+        }
+
+       
+
+        
+    }
+}
